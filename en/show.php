70952bee73c2cb4f2aed7a0530074b748f174c25
akkara27 <disoder@windowslive.com>
2016-07-18 16:32:03 +0700

    Edit show.php and set Ordinal Number

--- en/show.php
+++ en/show.php
@@ -1,80 +1,168 @@
 <?php
 session_start();
 ?>
+<!DOCTYPE html>
 <html>
-<head>
-<title>ThaiCreate.Com</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<head>
+		<title>Samfa Craftman</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<link rel="stylesheet" type="text/css" href="../css/style.css">
+		<script type="text/javascript" src="../js/jquery-1.10.2.js"></script>
+		<script type="text/javascript" src="https://www.google.com/cse/tools/onthefly?form=searchbox_demo&lang="></script>
+		<script type="text/javascript" src="modernizr.custom.79639.js"></script>
+		<link href='http://fonts.googleapis.com/css?family=PT+Sans+Narrow' rel='stylesheet' type='text/css'>
+		<script src="../js/jquery-1.11.0.min.js"></script>
+		<script src="../js/lightbox.min.js"></script>
+		<link href="../css/lightbox.css" rel="stylesheet" />
+        
+
+		<style type="text/css">
+			hr.style-head:after{
+					content: 'Check Confirm!';
+			}
+			#menu4{font-weight:bold;}
+		</style>
+
 </head>
-<?php
 
-if(!isset($_SESSION["intLine"]))
-{
-	echo "Cart Empty";
-	exit();
-}
+<body>
+	    <div id="container">
+	    	<div class="bg" id="item_51"></div>
+	    	
+	    	<div data-import="header.html">กรุณารอซักครู่...</div>
+			
+			<div id="DATA">
 
-include('../include/config.inc.php');
+				<div id="blog" class="order">
 
+		        	<div id="Detail" style="min-height: 710px;">
+		        		<hr class="style-head" >
+						<hr class="selectLine">
+
+	                                <?php
+for($i=0;$i<=(int)$_SESSION["intLine"];$i++)
+{ // Loop Order
 ?>
-  <form action="update.php" method="post">
-<table width="400"  border="1">
-  <tr>
-    <td width="101">ImgDoor</td>
-    <td width="82">Height</td>
-    <td width="82">Width</td>
-    <td width="79">Qty</td>
-    <td width="79">Acsr1</td>
-    <td width="79">Acsr2</td>
-    <td width="79">Acsr3</td>
-    <td width="79">Wood for Door</td>
-    <td width="79">Wood for Framing</td>
-    <td width="10">Del</td>
-  </tr>
-  <?php
-  $Total = 0;
-  $SumTotal = 0;
+	<div id="order2">
+	<div id="box-product">
+	<div id="model-product" style="width:50%;" >
+<?php
+	$door = $_SESSION["ImgDoor"][$i];
+	$x =  $_SESSION["Height"][$i];
+	$y =  $_SESSION["Width"][$i];
+	$quan =  $_SESSION["Quantity"][$i];
+	$acsr1 =  $_SESSION["Acsr1"][$i];
+	$acsr2 =  $_SESSION["Acsr2"][$i];
+	$acsr3 =  $_SESSION["Acsr3"][$i];
+	$wood_c = $_SESSION["WoodForDoor"][$i];
+	$frame_c = $_SESSION["WoodForFraming"][$i];
 
-  for($i=0;$i<=(int)$_SESSION["intLine"];$i++)
-  {
-	  ?>
-	  <tr>
-		<td><?=$_SESSION["ImgDoor"][$i];?></td>
-		<td><?=$_SESSION["Height"][$i];?></td>
-		<td><?=$_SESSION["Width"][$i];?></td>
-        <td><?=$_SESSION["Quantity"][$i];?></td>
-        <td><?=$_SESSION["Acsr1"][$i];?></td>
-        <td><?=$_SESSION["Acsr2"][$i];?></td>
-        <td><?=$_SESSION["Acsr3"][$i];?></td>
-        <td><?=$_SESSION["WoodForDoor"][$i];?></td>
-        <td><?=$_SESSION["WoodForFraming"][$i];?></td>
-		<td><a href="delete.php?Line=<?=$i;?>">x</a></td>
-	  </tr>
-	  <?php
-  }
-  ?>
-</table>
-<br>
-<table width="400"  border="0">
-  <tr>
-  <td><input type="submit" value="Update"></td>
-  <td align="right">Sum Total <?php echo number_format($SumTotal,2);?></td>
-  </tr>
-  </table>
-</form>
-<br><a href="madetocatalogue.html">Go to Product</a>
-| <a href="form.php">Checkout</a>
-<?php
-	if($SumTotal >= 0)
-	{
-?>
-	| <a href="clear.php">Clear</a>
-<?php
+	$pic_name = substr($door,0,2);
+	if($pic_name == "CS"){
+	$img_door  = '<img src="../pic_door_classic/'.$door.'.jpg" alt="door" >';
+	$img_door .= '<h5>'.$door.'</h5>';
+	echo $img_door;
+	$link = "classic.php";
+	}
+	else if ($pic_name == "CT"){
+	$img_door  = '<img src="../pic_door_contemporary/'.$door.'.jpg" alt="door" >';
+	$img_door .= '<h5>'.$door.'</h5>';
+	echo $img_door;
+	$link = "contemporary.php";
+	}
+	else if($pic_name == "MD"){
+	$img_door  = '<img src="../pic_door_modern/'.$door.'.jpg" alt="door" >';
+	$img_door .= '<h5>'.$door.'</h5>';
+	echo $img_door;
+	$link = "modern.php";
+	}
+	else{
+	$img_door  = '<img src="../pic_door_order/'.$door.'.jpg" alt="door" >';
+	$img_door .= '<h5>'.$door_order.'</h5>';
+	echo $img_door;
 	}
 ?>
-<?php
+</div>
+<div id="detail-product" style="width:50%;">
+<?php 
+//Ordinal Number
+$number = $i+1;
+$ends = array('th','st','nd','rd','th','th','th','th','th','th');
+if (($number%100) >= 11 && ($number%100) <= 13)
+   $abbreviation = $number. 'th';
+else
+   $abbreviation = $number. $ends[$number % 10];
+  
 ?>
+<h3><?=$abbreviation?></h3>
+	<div class="detail-form">
+	<label>Size</label>
+	<label>Height : <?=$x?> Cm</label>
+	<label>Width : <?=$y?> Cm</label>
+	<label>Quantity : <?=$quan?> Pcs</label>
+	</div>
+			<div class="detail-form">
+	        <label>Accessories</label>
+	        <?php
+													
+			if($acsr1 != ""){
+				echo '<label><input type="checkbox" checked id="acsr" name="Acsr1" value="Door/Windows disabled readonly"><span>Door/Windows</span></label>';
+			}
+			else{
+				echo '<label><input type="checkbox" disabled id="acsr" name="Acsr1" value="Door/Windows" disabled readonly><span>Door/Windows</span></label>';
+			}
+													
+			if($acsr2 != ""){
+				echo '<label><input type="checkbox" checked id="acsr" name="Acsr2" value="Frame" disabled readonly><span>Frame</span></label>';
+			}
+			else{
+				echo '<label><input type="checkbox" disabled id="acsr" name="Acsr2" value="Door/Windows" disabled readonly><span>Frame</span></label>';
+			}
+													
+			if ($acsr3 != ""){
+				echo '<label><input type="checkbox" checked id="acsr" name="Acsr3" value="Glass" disabled readonly><span>Glass work</span></label>';
+			}
+			else{
+				echo '<label><input type="checkbox" disabled id="acsr" name="Acsr3" value="Door/Windows" disabled readonly><span>Glass work</span></label>';
+			}
+			
+			?>
+			</div>
+											
+			<div class="option-product" onclick="$('#color').show();$('#style').hide();">
+				<label>Wood</label>
+				<label><img style="width:77px; border:3px solid black;" src="../wood_color/<?=$wood_c.".jpg"?>" alt="ไม้"></label>
+				</div>
+			<div class="option-product" onclick="$('#color').hide();$('#style').show();">
+		<label>Framing</label>
+		<label><img style="width:77px; border:3px solid black;" src="../wood_frame/<?=$frame_c.".jpg"?>" alt="ไม้"></label>
+	</div>
+</div><!--/*end detail-product*/-->
+</div><!--/*end box-product*/-->
+</div><!--/*end Detail*/-->
+<?php
+  } // End Loop Order
+?>
+		        	<hr class="style-End">
+					<div id="footer">
+						<a href="javascript:history.back(1);">< Back</a>
+	                    <a href="form.php">Confirm ></a>
+		        	</div>
+			  	</div><!--/*end blog*/-->
+				<div id="footerPan">
+					<p>
+						<a href="profile.html">COMPANY PROFILE</a> | 
+						<a href="woodis.html">WOOD IS?</a> | 
+						<a href="gallery.html">GALLERY</a> | 
+						<a href="order.html">ORDER US!</a> | 
+						<a href="contact.html">CONTACTS US</a>
+						<br/>
+						<span>Copyright &copy; 2015 Samfah Craftman Co., Ltd, All rights reserved.</span>
+					</p>
+		        </div><!--/*end footer*/-->
+
+			</div><!--/*end DATA*/-->
+
+		</div><!--/*end container*/-->
 </body>
 </html>
-
-<?php /* This code download from www.ThaiCreate.Com */ ?>
