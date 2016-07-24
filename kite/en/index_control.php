6b3d87a273d15436a0444a41fc51af903369cb1a
akkara27 <disoder@windowslive.com>
2016-07-24 17:04:53 +0700

    index_control 3 Pages
    1.ourstory
    2.gallery
    3.order us

---
+++ kite/en/index_control.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+	<title>Samfa</title>
+    
+    <!-- Bootstrap Core CSS -->
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- Full-Slider CSS -->
+	<link rel="stylesheet" type="text/css" href="../assets/css/full-slider.css">
+    
+    <!-- Jquery Core script -->
+	<script type="text/javascript" src="../../js/jquery-1.10.2.js"></script>
+    
+    
+    
+</head>
+<body>
+
+    <!-- Full Page Image Background Carousel Header -->
+    <div id="myCarousel" class="carousel slide">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for Slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <!-- Set Slide Ourstory -->
+                <div class="fill">
+            	<?php require_once("ourstory.html"); ?>
+            	</div>
+            </div>
+            <div class="item">
+                <!-- Set Slide Gallery -->
+                <div class="fill">
+                <?php require_once("gallery.html"); ?>
+                </div>
+            </div>
+            <div class="item">
+                <!-- Set Slide Order Us -->
+                <div class="fill">
+                <?php require_once("order.html"); ?>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+
+</body>
+
+    <!-- jQuery -->
+    <script src="../assets/js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../assets/js/bootstrap.min.js"></script>
+    
+</html>
