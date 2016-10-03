e60d1b950593084084d6a21ae14de5504a8a8cfc
krit99 <krit99@users.noreply.github.com>
2016-10-03 07:56:33 +0700

    update

---
+++ coming.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+	<title>Samfa</title>
+    
+    <!-- Bootstrap  -->
+	<link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+	<link href="../assets/css/font-awesome.min.css" rel="stylesheet">
+    <!-- CSS  -->
+	<link href="../assets/css/style.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../assets/css/ourstory.css" />
+    <link href="../assets/css/slick.css" rel="stylesheet">
+    <link href="../assets/css/full-slider.css" rel="stylesheet">
+    <!-- ICON  -->
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+</head>
+<body>
+
+    <div class="samfah-cover-ourstory-re">
+    <img src="../images/background/order.jpg" class="image-custom">
+    <div class="bg-dot">
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2" class="active-1"></li>
+            </ol>
+        </div>
+    <div class="samfah-cover-ourstory-abb">
+        <div class="samfah-cover-ourstory-table">
+            <div class="samfah-cover-ourstory-tc samfah-cover-ourstory-vl">
+                <div class="samfah-text-in-cover">
+                    <h1>COMING SOON</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    <!-- end hover gallery show for medium , small / mobile , tablet -->
+<!-- contact show large / desktop -->
+<div class="background-contact">
+    <div class="border-contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-xs-12">
+                    <div class="samfah-heading-contact">
+                        <h1>CONTACT US</h1>
+                    </div>
+                    <div class="address-1 ff">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1894.2743983651633!2d100.38671036605916!3d13.977195163950775!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e28f2d53f2b8db%3A0xa07865089c9b4726!2z4Lia4Lij4Li04Lip4Lix4LiXIOC5geC4i-C4oeC4n-C5ieC4suC4nOC4peC4tOC4leC4oOC4seC4k-C4keC5jOC5hOC4oeC5iSDguIjguLPguIHguLHguJQ!5e1!3m2!1sen!2sth!4v1471753922712" width="100%" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    </div>
+                    <div class="address-2 ff">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2011.321487937505!2d100.50483921723284!3d13.753041479612898!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e299165297b7e7%3A0x68aab2899e66498b!2z4LmB4LiL4Lih4Lif4LmJ4LiyIOC4geC4suC4o-C4iuC5iOC4suC4hw!5e1!3m2!1sth!2sth!4v1471764042023" width="100%" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    </div>
+                    <div class="samfah-botton-contact">
+                        
+                        <p><span class="botton-ra" id="dd"></span>โชว์รูม : <span class="number-font">91</span> หมู่ที่ <span class="number-font">8</span> ถนนตลิ่งชัน-สุพรรณบุรี ตำบลละหาร อำเภอบางบัวทอง จังหวัดนนทบุรี <span class="number-font">11110</span> <br /><span class="indent">เบอร์โทรศัพท์ </span><span class="number-font">081-838-1368 , 085-920-9636</span></p>
+                        
+                        
+                        <p><span class="botton-ra" id="dd-2"></span>โชว์รูม : <span class="number-font">84-88</span> ถนนบริพัตร แขวงบ้านบาตร เขตป้อมปราบศัตรูพ่าย จังหวัดกรุงเทพมหานคร <span class="number-font">10100</span> <br /><span class="indent">เบอร์โทรศัพท์ </span><span class="number-font">081-838-1368 , 085-920-9636</span></p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="samfah-socile">
+            <a href=""><i class="fa fa-facebook" aria-hidden="true"></i></a>
+            <a href=""><i class="fa fa-instagram" aria-hidden="true"></i></a>
+            <a href=""><i class="fa fa-twitter" aria-hidden="true"></i></a>
+        </div>
+    </div>
+</div>
+<!-- end contact show large / desktop -->
+	<!-- jQuery Library -->
+    <script type="text/javascript" src="../assets/js/jquery-2.1.0.min.js"></script>
+    <!-- Modernizr js -->
+    <script type="text/javascript" src="../assets/js/modernizr-2.8.0.min.js"></script>
+    <!-- Plugins -->
+    <script type="text/javascript" src="../assets/js/plugins.js"></script>
+    <!-- Custom JavaScript Functions -->
+    <script type="text/javascript" src="../assets/js/functions.js"></script>
+    <script type="text/javascript" src="../assets/js/masonry.pkgd.min.js"></script>
+    <!-- Masonry js -->
+    <script type="text/javascript" src="../assets/js/jquery.ajaxchimp.min.js"></script>
+    <!-- Ajax js -->
+    <script type="text/javascript" src="../assets/flip/jquery.flip.min.js"></script>
+    <!-- Flip js -->
+</body>
+</html>
